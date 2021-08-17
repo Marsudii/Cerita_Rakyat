76a6a6edc31c33e164793893d66a30daf86a3068
Marsudi <49959757+Marsudii@users.noreply.github.com>
2021-08-17 12:48:28 +0700

    Update index.js

--- src/components/RawaPening/index.js
+++ src/components/RawaPening/index.js
@@ -5,30 +5,76 @@ const RawaPening = () => {
         <div className="RawaPening">
             <h1>Rawa Pening</h1>
             <p>
-            Pada zaman dahulu, hiduplah seorang anak yang sakti. Kesaktiannya ini membuat seorang menyihir jahat iri. Penyihir jahat menyihir anak itu, sehingga tubuhnya penuh luka dengan bau yang sangat menyengat. Luka-luka baru akan muncul begitu luka lama mulai kering. Keadaannya kondisi tubuhnya itu, tidak ada seorang pun yang mau berhubungan dengannya. Jangankan bertegur sapa, berdekatan saja orang tidak mau. Mereka takut tertular.
-           </p>
-            <br/>
-            <p>
-            Suatu hari, anak ini bermimpi ada seorang perempuan tua yang dapat menyembuhkan penyakitnya. Ia pun berkelana mencari perempuan tua dalam mimpinya tersebut. Di setiap kampung yang ia datangi, ia selalu ditolak oleh penduduk. Mereka merasa jijik dan mengusir anak ini. Akhirnya, sampailah ia di sebuah kampung yang sebagian besar penduduknya adalah orang-orang yang sombong. Tidak banyak orang yang miskin di desa itu. Mereka akan diusir atau dibuat tidak nyaman kalau tinggal di sana. Hal ini mengusik hati anak kecil ini.
-            </p>
-            <br/>
-            <p>
-            Pada sebuah pesta yang diselenggarakan di kampung itu, anak kecil ini berhasil masuk. Namun, orang-orang segera mengusirnya dan mencaci-makinya. Ia langsung diseret keluar. Pada saat terseret, ia berpesan kepada orang-orang itu supaya lebih memerhatikan orang tak punya. Mendengar kata-kata anak itu, beberapa orang makin marah, bahkan meludahinya sambil berkata, “Dasar anak setan, anak buruk rupa!”
-            </p>
-            <br/>
-            <p>
-            Anak itu merasa terluka dengan perlakuan orang-orang tersebut. Lalu, ia menancapkan sebuah lidi di tanah don berkata, “Tak ada satu pun yang bisa mencabut lidi ini dari tanah, hanya aku yang bisa melakukannya!” Orang-orang meragukan ucapan anak tersebut. Mereka pun mencoba mencabut lidi tersebut. Namun, tak seorangpun dapat melakukannya. Dalam beberapa hari, lidi itu tak bisa tercabut. Suatu hari, secara diam-diam, anak itu datang don mencabut lidi itu. Tanpa sepengetahuannya, ada seorang warga yang melihatnya dan melaporkannya kepada warga yang lain.  
-            </p>
-            <br/>
-            <p>
-            Dari tempat lidi itu dicabut, mengalirlah mata air. Semakin lama, air itu semakin deras. Air menenggelamkan daerah tersebut, sehingga menjadi sebuah telaga yang kini bernama Telaga Rawa Pening. Tidak ada yang selamat dari musibah itu kecuali seorang perempuan tua yang berbaik hati memberinya tempat tinggal dan merawatnya. Secara ajaib penyakit kulit anak itu sembuh. Namun, penyihir jahat yang telah menyihir si anak itu tidak terima dengan kesembuhan itu. Kemudian, ia menyihir anak itu menjadi seekor ular besar dengan sebuah kalung genta di lehernya. Konon, ular ini sering keluar dari sarangnya pada tengah malam. Setiap kali bergerak, dentingan kalung di lehernya selalu berbunyi klentang-klenting. Bunyi inilah yang kemudian membuatnya dinamakan Baru Klinting. Kemunculan ular itu diyakinin masyarakat sebagai tando keberuntungan bagi nelayan nelayan yang tidak mendapat ikan.
-            </p>
-            <br/>
-            <p>
-            Kini, Telaga Rawa Pening adalah objek wisata yang sangat populer di Jawa Tengah. Tempat ini terletak di Desa Bukit Cinta, Kabupaten Ambarawa.
-            </p>
-            <br/>
-            <strong>Tokoh dalam cerita Rawa Pening:</strong>
+        <img
+          src={`https://1.bp.blogspot.com/-fCOOZhVCdng/YRtMq1uruAI/AAAAAAAAAjE/tluNu57S7wIbbVqQDw_MzI2bldajPl2agCLcBGAsYHQ/s1024/1.jpg`}
+          alt="rawa pening"
+        />
+        </p>
+<br/>
+              <p>
+        <img
+          src={`https://1.bp.blogspot.com/-nknzdU5OwG0/YRtMrGQyCVI/AAAAAAAAAjM/A4Fje8yco_k1N6Rr-BY3uxZxmq9fBmIogCLcBGAsYHQ/s1024/2.jpg`}
+          alt="rawa pening"
+        />
+        </p>
+<br/>
+     <p>
+        <img
+          src={`https://1.bp.blogspot.com/-SGCcSIPtsuA/YRtMr0qQEfI/AAAAAAAAAjQ/q1fQC6G6nBU2OYZJNZg4YNaI7ox8UfKVQCLcBGAsYHQ/s1024/3.jpg`}
+          alt="rawa pening"
+        />
+        </p>
+<br/>
+              <p>
+        <img
+          src={`https://1.bp.blogspot.com/-8Dm1cnGUQ4o/YRtMsVWO8gI/AAAAAAAAAjU/fnkqdeoVQ14H7IsEYdINu8wIxfxglq8UgCLcBGAsYHQ/s1024/4.jpg`}
+          alt="rawa pening"
+        />
+        </p>
+<br/>
+              <p>
+        <img
+          src={`https://1.bp.blogspot.com/-2gzki2UjnHE/YRtMscmB6aI/AAAAAAAAAjY/3hRpyAccko8G41NyBJy-d_rXGnDGzq_mQCLcBGAsYHQ/s1024/5.jpg`}
+          alt="rawa pening"
+        />
+        </p>
+<br/>
+              <p>
+        <img
+          src={`https://1.bp.blogspot.com/-kKYXF19hqB4/YRtMs4RpC0I/AAAAAAAAAjc/YeJZBsnzZDEmqSN0C9xmwB01d68Pg7ArgCLcBGAsYHQ/s1024/6.jpg`}
+          alt="rawa pening"
+        />
+        </p>
+<br/>
+              <p>
+        <img
+          src={`https://1.bp.blogspot.com/-x3_I0E67x6k/YRtMtfywanI/AAAAAAAAAjg/rlK9prAVzpcVABRgoIMtlR_NDVU08QYxgCLcBGAsYHQ/s1024/7.jpg`}
+          alt="rawa pening"
+        />
+        </p>
+<br/>
+              <p>
+        <img
+          src={`https://1.bp.blogspot.com/-whuIfq5_uBc/YRtMtsiUTII/AAAAAAAAAjk/gbOgzDknZt4zJec1KYg5hzA5bwNDlXjUwCLcBGAsYHQ/s1024/8.jpg`}
+          alt="rawa pening"
+        />
+        </p>
+<br/>
+              <p>
+        <img
+          src={`https://1.bp.blogspot.com/-BIlCu1AbWbQ/YRtMt-I6ITI/AAAAAAAAAjo/CSbk9J7a9Dogjs40jESp9HYFyQywdgs9wCLcBGAsYHQ/s1024/9.jpg`}
+          alt="rawa pening"
+        />
+        </p>
+<br/>
+              <p>
+        <img
+          src={`https://1.bp.blogspot.com/-Pk_vdn1pu-Q/YRtMq6hPjSI/AAAAAAAAAjI/JLr3j0GIxccBVD7U0oydQYrw6LJZ6a_AwCLcBGAsYHQ/s1024/10.jpg`}
+          alt="rawa pening"
+        />
+        </p>
+<br/>
+              <strong>Tokoh dalam cerita Rawa Pening:</strong>
             <h4>1. Ki Hajar (Bapak Baru Klinting)</h4>
             <h4>2. Nyai Selakanta (Ibu Baru Klinting)</h4>
             <h4>3. Naga (Baru Klinting)</h4>
