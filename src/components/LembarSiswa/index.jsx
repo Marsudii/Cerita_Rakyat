728679176848c5555ff8bcb9bc7d59de5c28cdb5
Marsudi <49959757+Marsudii@users.noreply.github.com>
2021-08-17 15:34:37 +0700

    Update index.jsx

--- src/components/LembarSiswa/index.jsx
+++ src/components/LembarSiswa/index.jsx
@@ -5,7 +5,7 @@ const LembarSiswa = () => {
     <div className="works">
       <h1>Lembar Siswa</h1>
       <br/>
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdIqq_JJAp0y5ShjCcRxhHMHOjXqiC4pKJJPZjXOyDJWuMv-A/viewform?embedded=true" width="375" height="667" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
+    <iframe title="Google Form" src="https://docs.google.com/forms/d/e/1FAIpQLSdIqq_JJAp0y5ShjCcRxhHMHOjXqiC4pKJJPZjXOyDJWuMv-A/viewform?embedded=true" width="375" height="667" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
     
     </div>
   );
