85ccaf731371f4d65a126a2d3b893205aaf802a6
Marsudi <49959757+Marsudii@users.noreply.github.com>
2021-08-17 15:33:42 +0700

    Update index.jsx

--- src/components/LembarSiswa/index.jsx
+++ src/components/LembarSiswa/index.jsx
@@ -6,6 +6,7 @@ const LembarSiswa = () => {
       <h1>Lembar Siswa</h1>
       <br/>
     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdIqq_JJAp0y5ShjCcRxhHMHOjXqiC4pKJJPZjXOyDJWuMv-A/viewform?embedded=true" width="375" height="667" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
+    
     </div>
   );
 };
